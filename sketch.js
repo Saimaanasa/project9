11986f1b88d1de14dcdfc4d8729bfb93327cde4a
Saimaanasa <87806207+Saimaanasa@users.noreply.github.com>
2021-07-28 15:41:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+if(keyIsDown(RIGHT_ARROW)) {
+    background("yellow");
+  }
+
+  if(keyIsDown(LEFT_ARROW)) {
+    background("pink");
+  }
+
+  if(keyIsDown(DOWN_ARROW)) {
+    background("red");
+  }
+  if(keyIsDown(UP_ARROW)) {
+    background("violet");
+  }
+  }
+  background(445);
+
+
+
+
+
+
